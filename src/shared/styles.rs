34347b98679f7fd8026f8e459eee68e12eaa07c1
Jorge Bejar <jorge@wyeworks.com>
2023-08-17 11:27:20 -0300

    Do not add fonts that are already added by Makepad

--- src/shared/styles.rs
+++ src/shared/styles.rs
@@ -3,23 +3,17 @@ use makepad_widgets::*;
 live_design! {
     TITLE_TEXT = {
         font_size: (14),
-        font: {path: dep("crate://self/resources/GoNotoKurrent-Regular.ttf")}
+        font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
     }
 
     REGULAR_TEXT = {
         font_size: (12),
-        font: {path: dep("crate://self/resources/GoNotoKurrent-Regular.ttf")}
+        font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
     }
 
     TEXT_SUB = {
         font_size: (FONT_SIZE_SUB),
-        font: {path: dep("crate://self/resources/GoNotoCurrent-Bold.ttf")}
-    }
-
-    TEXT_P = {
-        font_size: (FONT_SIZE_P),
-        height_factor: 1.65,
-        font: {path: dep("crate://self/resources/GoNotoKurrent-Regular.ttf")}
+        font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
     }
 
     COLOR_PROFILE_CIRCLE = #xfff8ee
