22f3bd408fefd213152cc13643ffba8a7f09f0f8
Julian Montes de Oca <julmontesdeoca@gmail.com>
2023-08-17 10:44:33 -0300

    Initial layout (scroll not working)

--- src/discover/mod.rs
+++ src/discover/mod.rs
@@ -1,2 +1,3 @@
 pub mod discover_screen;
 pub mod moments_screen;
+pub mod moment_list;
--- src/discover/moments_screen.rs
+++ src/discover/moments_screen.rs
@@ -3,12 +3,14 @@ use makepad_widgets::*;
 
 live_design! {
     import makepad_widgets::frame::*;
+    import crate::discover::moment_list::MomentList;
 
     MomentsScreen = <Frame> {
         walk: { width: Fill, height: Fill }
         show_bg: true
         draw_bg: {
-            color: #ddd
+            color: #fff
         }
+        <MomentList> {}
     }
 }
